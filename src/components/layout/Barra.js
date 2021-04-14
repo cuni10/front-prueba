e4a45d2503a7d4511e1c7ff67bfb96c3a7754445
cuni10 <cuticraft@gmail.com>
2021-04-14 08:36:14 -0300

    Editando css barra.js

--- src/components/layout/Barra.js
+++ src/components/layout/Barra.js
@@ -3,14 +3,18 @@ import React from 'react';
 const Barra = () => {
     return ( 
         <div className="row">
-            <div className="navbar barra">
-                <div className="container-fluid">
+            <div className="navbar barra flex-row">
+                <div className="container-fluid ">
                     <h1 className="navbar-brand">Juegos.com</h1>
-                    <form className="d-flex">
+                    <form className="d-flex ">
                         <input type="search" className="form-control me-2" placeholder="¿Que Buscas?"/>
-                        <button class="btn btn-outline-success" type="submit">Buscar</button>
+                        <button class="btn btn-outline-light" type="submit">Buscar</button>
                     </form>
-                    <button className="btn ">Cerrar sesion</button>
+                    <div className="d-inline-flex">
+                        <button className="btn btn-outline-light m-1">Cuni Gramajo</button>
+                        <button className="btn btn-outline-light m-1">Cerrar sesion</button>
+                    </div>
+                    
                 </div>
             </div>
         </div>
